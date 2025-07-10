29f44376e27234697c3b5c3a13487644d57854c0
Odee Jairo <odee.jairo@gmail.com>
2025-07-10 18:57:02 +0300

    Wrote a Class named StudentClass:

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -10,3 +10,35 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+
+// Interface describing the methods the student must have
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  // Interface describing how the constructor should work
+  interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+  }
+  
+  // Actual class
+  class StudentClass implements StudentClassInterface {
+    constructor(private firstName: string, private lastName: string) {}
+  
+    workOnHomework(): string {
+      return "Currently working";
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
